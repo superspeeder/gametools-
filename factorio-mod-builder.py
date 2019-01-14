c4a5783581b5af3d3c2043a03230bc8168bfeb8e
superspeeder <andy@delusion.org>
2019-01-14 12:10:22 -0800

    Update factorio-mod-builder.py

--- factorio-mod-builder.py
+++ factorio-mod-builder.py
@@ -1,1 +1,20 @@
+class Prototype:
+  def __init__(self, data):
+    self.data = data
 
+validation = {
+  "type": ("must-be-in", ("item", "recipe", "entity", "item-subgroup", "item-group", "recipe-category", "assembly-machine", "inserter")),
+  "name": ("must-be-type", "string")
+  "icon": ("must-be-type-custom", "path")
+  "icon-size": ("must-be-type", "int")
+  "ingredients": ("must-fit-format", IngredientList.gatherFormat())
+}
+
+recipe = PrototypeType(MultipleFormatDefinitions(recipe, recipe_with_mult_outputs), ExtraData(("load-order-index", Placeholder(int)), ("allow-extra-data", True)))
+
+
+
+
+
+
+def validate(key, val)
